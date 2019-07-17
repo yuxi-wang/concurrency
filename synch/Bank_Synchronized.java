a3e57179e32b285a85b08ae65a140764c6d390cb
yuxi-wang <52993579+yuxi-wang@users.noreply.github.com>
2019-07-17 15:16:08 +0800

    Create Bank_Synchronized.java

---
+++ synch/Bank_Synchronized.java
@@ -0,0 +1,38 @@
+package yuxiGroup.yuxiArtifact.concurrency.synch;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank_Synchronized {
+	private final double[] accounts;
+	
+	public Bank_Synchronized (int n, double initialBalance) {
+		accounts = new double[n];
+		Arrays.fill(accounts, initialBalance);
+	}
+	
+	public synchronized void transfer(int from, int to, double amount) throws InterruptedException{
+
+		while (accounts[from] < amount ) 
+			wait();
+		System.out.print(Thread.currentThread());
+		accounts[from] -= amount;
+		System.out.printf(" %10.2f from %d to %d", amount, from ,to);
+		accounts[to] += amount;
+		System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+			
+			notifyAll();
+
+	}
+
+	public synchronized double getTotalBalance() {
+		double sum = 0;
+		for (double d :accounts)
+			sum += d;
+		return sum;
+	}
+	
+	public synchronized int size() {
+		return accounts.length;
+	}
+}
